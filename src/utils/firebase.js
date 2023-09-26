adc4de2812bdd766b4656bd221518668418ea723
cxw0520 <james30088@gmail.com>
2023-09-26 23:57:58 +0800

    1

---
+++ src/utils/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAUlqVFqNfKTY34UCA0_jt9kHlTqMgDzi0",
+    authDomain: "ar-classroom-1bb48.firebaseapp.com",
+    projectId: "ar-classroom-1bb48",
+    storageBucket: "ar-classroom-1bb48.appspot.com",
+    messagingSenderId: "801778279327",
+    appId: "1:801778279327:web:e63f67b115057a5e2c53aa",
+    measurementId: "G-DEC44C4SPG"
+  };
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase;
